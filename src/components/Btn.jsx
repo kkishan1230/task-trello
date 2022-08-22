9b0073bbfb696d17034acc0bc23ad721c59c1d40
kkishan1230 <kkishan1230@gmail.com>
2022-08-22 14:59:09 +0530

    deleted some stuff

---
+++ src/components/Btn.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+function Btn() {
+  return (
+    <div>
+      <Button>Kishan</Button>
+    </div>
+  );
+}
+
+export default Btn;
